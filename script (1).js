7b89fbae8f24255b9adb19e6af1188606c557127
kilton biswas <145213319+klintonbis@users.noreply.github.com>
2024-09-23 21:23:18 +0600

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+// Next/previous controls
+function plusSlides(n) {
+    slideIndex += n;
+    let slides = document.getElementsByClassName("slides");
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    if (slideIndex < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
